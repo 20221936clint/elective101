77dea0ed00bd376ca2978732899fd091633b0140
link <link@gmail.com>
2023-12-12 07:00:27 +0700

    web

---
+++ views/script.js
@@ -0,0 +1,31 @@
+function addInteractiveNavbar() {
+    // Code for the interactive navbar
+}
+
+function updateNavbarActiveState() {
+    // Code for updating the active state of the navbar items
+}
+
+function fetchAndDisplayProjectDetails() {
+    // Code for fetching and displaying project details
+}
+
+function setupScrollEffects() {
+    // Code for setting up scroll effects
+}
+
+function addImageLightbox() {
+    // Code for adding an image lightbox
+}
+
+function initializeAnimations() {
+    // Code for initializing animations
+}
+
+// Call the functions to set up the page
+addInteractiveNavbar();
+updateNavbarActiveState();
+fetchAndDisplayProjectDetails();
+setupScrollEffects();
+addImageLightbox();
+initializeAnimations();
